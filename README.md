a569c1cff1502cf14555548e7d4a5d11a531f3a4
Odysseas Chryssos <ody.chryssos@gmail.com>
2025-10-26 23:32:46 +0200

    Added bunch of stuff. mostly green floor with stone in some random stone here and there :3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CodeLab-Talos-2025-2026-proj1
+
+this is an epic uncontional topdown 1v1v1v1 free for all rpg stiile arena game!
+
+developed by odysseas chrysssos, Antonis smth & Sotiris Tatsilis
--- main.py
+++ main.py
@@ -5,18 +5,23 @@ Cool Greek Games Studio
 """
 import pygame 
 import random
+import os
 
+# === Setup ===
 screen = pygame.display.set_mode((400,800))
 pygame.display.toggle_fullscreen()
 running = True
 
+# === Variabled ===
+tileSize = 50
 tileTypeTextures = {
-    "grass": "path",
-    "stone": "path",
+    "grass": "./media/grass.png",
+    "stone": "./media/stone.png",
     "wood": "path",
     "wall": "path"
 }
 
+# === Classes ===
 class Weapon():
     def __init__(self, streangth, name, texture):
         self.streangth = streangth
@@ -24,41 +29,74 @@ class Weapon():
         self.texture = texture
         
 class Entity():
-    def __init__(self, speed, strength, durabillity, weapon, secondary_weapon, shield, armor ):
+    def __init__(self, health, speed, strength, durabillity, weapon, secondary_weapon, shield, armor):
         self.strength = strength  
         self.durabillity = durabillity
         self.weapon = weapon
         self.secondary_weapon = secondary_weapon
         self.shield = shield
         self.armor = armor
+        self.speed = speed
+        self.health = health
 
     def attack(self, weapon, victim):
         if weapon == 1:
             weapon = self.weapon
         elif weapon == 2:
             weapon = self.secondary_weapon
-    
+        
+         # Kai ean theloume prevent cheating k kala auto kanonika sto server (opos k genika to entity management)
         damageDealt = weapon.strength * (self.streangth / random.randint(0,1))
     
-        victim.takeDamage(damageDealt)
+        victim.takeDamage(damageDealt) # Auto einai gia offline. gia online the prepi na stelnoume packet!
+    
+    def takeDamage(self, damageToBeDealt):
+        self.health =- damageToBeDealt
 
 class Tile():
-    def init(self, typee, wall, positionX, positionY):
+    def __init__(self, typee, wall, positionX, positionY):
 
         self.passable = not wall
         self.texture = tileTypeTextures[typee]
         self.posX = positionX
         self.posY = positionY
 
-        self.surface = pygame.Surface((20, 20), pygame.SRCALPHA)
-        self.surface.fill("orange")
+
+        texture_path = tileTypeTextures.get(typee)
+        texture_path = os.path.join(os.path.dirname(__file__), texture_path)
+        print(texture_path)
+        if texture_path and os.path.exists(texture_path):
+            # Load and scale texture
+            self.surface = pygame.image.load(texture_path).convert_alpha()
+            self.surface = pygame.transform.scale(self.surface, (50, 50))
+        else:
+            # fallback if missing
+            self.surface = pygame.Surface((50, 50), pygame.SRCALPHA)
+            self.surface.fill("blue")
 
         self.rect = self.surface.get_rect(topleft=(self.posX, self.posY))
     def draw(self):
         screen.blit(self.surface, self.rect)
 
-test = Tile("grass", False, 10, 10)
+# === Server Communication ===
+def sendPacket(packetId):
+    pass
 
+# === Object Setup ===
+
+# == Tile setup ==
+grassFloor = []
+
+for i in range(0, 40):
+    for j in range(0,16):
+        if j < 8: tile = "grass" 
+        else: tile = "stone"
+        grassFloor.append(Tile(tile, False, i * 50, j * 50))
+
+# == Entity setup ===
+pass
+
+# === Main Game loop ====
 while running:
     pygame.display.flip()
     screen.fill("red")
@@ -66,7 +104,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.key.K_ESCAPE:
+            if event.key == pygame.K_ESCAPE:
                 pygame.quit()
 
-    test.draw()
+    # === Object Drawing ===
+    for grass in grassFloor:
+        grass.draw()
